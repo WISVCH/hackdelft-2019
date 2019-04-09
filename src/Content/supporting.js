9836b403e95951992832d239f45e4f429401fabe
Thijs Nederlof <pnede3@users.noreply.github.com>
2019-04-09 13:04:40 +0200

    Fixing supporting partners
    Removed red bull (for now)
    added sticker mule
    Removed X for now because they don't have a normal logo
    kept MLH
    kept Picnic

--- src/Content/supporting.js
+++ src/Content/supporting.js
@@ -9,15 +9,15 @@ const supporting = [
     title: 'Picnic',
     href: 'https://www.picnic.nl'
   },
-  {
-    asset: 'assets/rb.png',
-    title: 'Red Bull',
-    href: 'https://www.redbull.com/nl-nl/'
-  },
-  {
-    asset: 'assets/tudSC.jpg',
-    title: 'Sports and Culture - TU Delft',
-    href: 'https://www.tudelft.nl/sc/'
+//   {
+//     asset: 'assets/tudSC.jpg',
+//     title: 'Sports and Culture - TU Delft',
+//     href: 'https://www.tudelft.nl/x/'
+//   },
+    {
+    asset: 'assets/sticker-mule.png',
+    title: 'Sticker Mule',
+    href: 'http://hackp.ac/mlh-stickermule-hackathons'
   }
 ];
 export default supporting;
